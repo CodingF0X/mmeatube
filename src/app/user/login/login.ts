49c9a8ec6a2d1ba0e4a792bd2cfd531debca2a0a
jaafar alkaales <jaafar.alkaales001@stud.fh-dortmund.de>
2025-12-07 09:04:13 +0100

    feat-- applied reactive forms to login component

--- src/app/user/login/login.ts
+++ src/app/user/login/login.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputComponent } from '../../shared/input-component/input-component';
+import { AlertComponent } from '../../shared/alert-component/alert-component';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, InputComponent, AlertComponent],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
+  standalone: true,
 })
 export class Login {
+  fb = inject(FormBuilder);
 
+  showAlert = signal<boolean>(false);
+  alertColor = signal<string>('blue');
+  alertMsg = signal<string>('Sugmitting...');
+
+  form = this.fb.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+
+  login() {
+    this.showAlert.set(true);
+    this.alertColor.set('blue');
+    setTimeout(() => {
+      this.showAlert.set(false);
+    }, 2000);
+  }
 }
